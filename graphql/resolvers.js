3855cd63b3e3f6cb90815300c30d07c1557c8343
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-02-21 02:37:12 +0900

    delete movie

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,4 @@
-import { getById, getMovies, addMovie } from "../DB/db";
+import { getById, getMovies, addMovie, deleteMovie } from "../DB/db";
 
 const resolvers = {
   Query: {
@@ -7,6 +7,7 @@ const resolvers = {
   },
   Mutation: {
     addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
