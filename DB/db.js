98f472130a3b3249ba1b2850dd5e702caffec281
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-02-21 02:56:12 +0900

    get movie with REST API

--- DB/db.js
+++ DB/db.js
@@ -1,46 +1,7 @@
-let movies = [
-  {
-    id: 1,
-    name: "abcd",
-    score: 2,
-  },
-  {
-    id: 2,
-    name: "qwert",
-    score: 19,
-  },
-  {
-    id: 3,
-    name: "CXZ<M",
-    score: 2,
-  },
-];
+import axios from "axios";
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.mx/api/v2/list_movies.json";
 
-export const getById = (id) => {
-  const filteredMovies = movies.filter((movie) => movie.id === id);
-  console.log(filteredMovies);
-  return filteredMovies[0];
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-
-  if (cleanedMovies.length < movies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: movies.length + 1,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+export const getMovies = (limit, rating) => {
+  axios.get(`${API_URL}`).then((res) => res.data.movies);
 };
